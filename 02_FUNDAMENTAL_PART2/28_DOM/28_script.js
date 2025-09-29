ec6315f27747c12f3be4e798702b8053c9fe3af2
Twinkal Pansuriya <twinklepansuriya4124@gmail.com>
2025-09-29 11:13:16 +0530

    Read Me

---
+++ 02_FUNDAMENTAL_PART2/28_DOM/28_script.js
@@ -0,0 +1,5 @@
+window.console.log(window);
+
+// this is not recommanded for this use the background only use for user action work
+document.body.style.background = "red";
+document.body.style.color = "yellow";
